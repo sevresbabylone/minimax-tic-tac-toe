81e22114a69020f68da5bdd91c8705e9070aed80
sevresbabylone <cheryl.francais@gmail.com>
2018-06-26 00:02:36 +0800

    adds animated grid

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var path = document.querySelector('.tic-tac-toe-line');
-var length = path.getTotalLength();
+var paths = document.querySelectorAll('.tic-tac-toe-line');
+var length = paths[0].getTotalLength();
 
 // Clear any previous transition
-path.style.transition = path.style.WebkitTransition = 'none';
+paths.forEach(function(path) {
+  path.style.transition = path.style.WebkitTransition = 'none';
 
-path.style.strokeDasharray = length + ' ' + length;
-path.style.strokeDashoffset = length;
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
 
-path.getBoundingClientRect();
-path.style.transition = path.style.WebkitTransition =
-  'stroke-dashoffset 0.5s linear';
-// Go!
-path.style.strokeDashoffset = '0';
+  path.getBoundingClientRect();
+  path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 0.4s linear';
+  // Go!
+  path.style.strokeDashoffset = '0';
+})
