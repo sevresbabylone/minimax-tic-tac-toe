8b4591ea964a03b15c7e743ef7ee38434046099d
sevresbabylone <cheryl.francais@gmail.com>
2018-06-27 02:48:47 +0800

    adds svg animations for symbols

--- script.js
+++ script.js
@@ -1,16 +1,76 @@
-var paths = document.querySelectorAll('.tic-tac-toe-line');
-var length = paths[0].getTotalLength();
+var paths = document.querySelectorAll('.tic-tac-toe-line')
 
-// Clear any previous transition
-paths.forEach(function(path) {
-  path.style.transition = path.style.WebkitTransition = 'none';
-
-  path.style.strokeDasharray = length + ' ' + length;
-  path.style.strokeDashoffset = length;
-
-  path.getBoundingClientRect();
-  path.style.transition = path.style.WebkitTransition =
-    'stroke-dashoffset 0.4s linear';
-  // Go!
-  path.style.strokeDashoffset = '0';
+paths.forEach(function (path) {
+  drawLine(path, '0')
 })
+
+var cells = document.getElementsByClassName('tic-tac-toe-cell')
+var restartButton = document.querySelector('.restart-button')
+restartButton.addEventListener('click', function () {
+  paths.forEach(function (path) {
+    redrawLine(path, '0')
+    drawLine(path, '0')
+    var symbols = document.querySelectorAll('.tic-tac-toe-cross-left, .tic-tac-toe-cross-right, .tic-tac-toe-nought')
+    symbols.forEach(function (symbol) {
+      symbol.style.display = 'none'
+    })
+  })
+}, false)
+
+function redrawLine (element, timingDelay) {
+  element.style.display = 'none'
+  element.style.strokeDashoffset = '120'
+}
+function drawLine (element, timingDelay) {
+  element.style.display = 'block'
+
+  element.transition = element.style.WebkitTransition = 'none'
+  element.getBoundingClientRect()
+  element.style.transition = element.style.WebkitTransition =
+    'stroke-dashoffset 0.3s linear'
+  element.style.transitionDelay = timingDelay
+  element.style.strokeDashoffset = '0'
+}
+var circle1 = document.querySelector('.tic-tac-toe-nought')
+
+Array.prototype.forEach.call(cells, function (cell) {
+  cell.addEventListener('click', function (event) {
+    drawLine(circle1, '0')
+    var crossesRight = document.querySelectorAll('.tic-tac-toe-cross-right')
+    var crossesLeft = document.querySelectorAll('.tic-tac-toe-cross-left')
+    crossesLeft.forEach(function (cross, index) {
+      drawLine(cross, '0')
+      drawLine(crossesRight[index], '0.3s')
+    })
+  })
+})
+
+function Session () {
+  this.xScore = 0
+  this.oScore = 0
+  this.currentGame = new Game()
+}
+
+function Game () {
+  this.board = [null, null, null, null, null, null, null, null]
+  this.currentPlayer = 'x'
+}
+
+Game.prototype.clearBoard = function () {
+  // zoom out of viewport
+  // fade opacity of lines
+  // after fade, switch out to old strokeDashoffset
+  // display another SVG stating WINNER or LOSER on top of it
+}
+Game.prototype.newBoard = function () {
+  // SVG message fades out / flicker
+  // redraws lines
+}
+Game.prototype.switchPlayer = function () {
+
+}
+Game.prototype.addMove = function () {
+
+}
+Game.WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
