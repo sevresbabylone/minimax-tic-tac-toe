f3e7a2738b840f14afec843c485f8fe0d5def09d
sevresbabylone <cheryl.francais@gmail.com>
2018-07-07 00:30:40 +0800

    completes alpha-beta pruning example

--- alpha-beta-pruning/alpha-beta.js
+++ alpha-beta-pruning/alpha-beta.js
@@ -19,35 +19,51 @@
 var nodeArray = [null, null, null, null, null, null, null, 3, 5, 6, 9, 1, 2, 0, -1]
 
 function minimax (scoreArray, currentIndex, isMaximisingPlayer, alpha, beta) {
+  // if it's a leaf
   if (currentIndex >= Math.floor(nodeArray.length / 2)) {
     scoreArray[currentIndex] = nodeArray[currentIndex]
     return currentIndex
   }
-  if (beta <= alpha) {
 
-  }
   var leftChildIndex = (2 * currentIndex) + 1
   var rightChildIndex = (2 * currentIndex) + 2
-
   // console.log('leftChildIndex', leftChildIndex)
   // console.log('rightChildIndex', rightChildIndex)
   var betterIndex
   if (isMaximisingPlayer) {
     minimax(scoreArray, leftChildIndex, !isMaximisingPlayer, alpha, beta)
-    minimax(scoreArray, rightChildIndex, !isMaximisingPlayer, alpha, beta)
-    betterIndex = scoreArray[leftChildIndex] >= scoreArray[rightChildIndex] ? leftChildIndex : rightChildIndex
-    alpha = Math.max(alpha, scoreArray[betterIndex])
+
+    alpha = Math.max(alpha, scoreArray[leftChildIndex])
+    betterIndex = leftChildIndex
+    if (!(beta <= alpha)) {
+      minimax(scoreArray, rightChildIndex, !isMaximisingPlayer, alpha, beta)
+      betterIndex = scoreArray[leftChildIndex] >= scoreArray[rightChildIndex] ? leftChildIndex : rightChildIndex
+    }
   } else {
     minimax(scoreArray, leftChildIndex, !isMaximisingPlayer, alpha, beta)
-    minimax(scoreArray, rightChildIndex, !isMaximisingPlayer, alpha, beta)
-    betterIndex = scoreArray[leftChildIndex] < scoreArray[rightChildIndex] ? leftChildIndex : rightChildIndex
-    beta = Math.min(beta, scoreArray[betterIndex])
+    beta = Math.min(beta, scoreArray[leftChildIndex])
+    betterIndex = leftChildIndex
+    if (!(beta <= alpha)) {
+      minimax(scoreArray, rightChildIndex, !isMaximisingPlayer, alpha, beta)
+      betterIndex = scoreArray[leftChildIndex] < scoreArray[rightChildIndex] ? leftChildIndex : rightChildIndex
+    }
   }
   scoreArray[currentIndex] = scoreArray[betterIndex]
   return betterIndex
 }
 
+function getBestMove (currentIndex, isMaximisingPlayer) {
+  var scoreArray = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+  var bestMove = minimax(scoreArray, currentIndex, isMaximisingPlayer, -10000, 10000)
+  // console.log(scoreArray)
+  return bestMove
+}
 // throwaway scoreArray
-var scoreArray = []
 // best move for maximizer at A (0) is to pick B (1)
-console.log(minimax(scoreArray, 0, true, -10000, 10000))
+// [ 5, 5, 0, 5, 9, 2, 0, 3, 5, 6, 9, 1, 2, 0, -1 ]
+
+console.log('Between 1 and 2, the best move for maximising player to make at position 0 is ' + getBestMove(1, true))
+// expected scoreArray after alpha beta pruning
+// [5, 5, 2, 5, 6, 2, null , 3, 5, 6, null, 1, 2, null, null]
+// expected bestMove => 1
+console.log('Between 5 and 6, the best move for minimising player to make at position 2 is ' + getBestMove(2, false))
